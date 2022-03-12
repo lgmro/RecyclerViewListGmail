b3809ddeef2df2f2c1a770dc875fa7aa80138985
lgmro <lucasgmmro@gmail.com>
2022-03-12 12:52:01 -0300

    Terminating ViewHolder

--- app/src/main/java/com/lgmro/recycleviewlistgmail/EmailAdapter.kt
+++ app/src/main/java/com/lgmro/recycleviewlistgmail/EmailAdapter.kt
@@ -1,9 +1,17 @@
 package com.lgmro.recycleviewlistgmail
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.Typeface.BOLD
+import android.graphics.Typeface.NORMAL
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.recyclerview.widget.RecyclerView
 import com.lgmro.recycleviewlistgmail.model.Email
 
@@ -14,7 +22,23 @@ class EmailAdapter(
         inner class EmailViewHolder(itemView: View) : RecyclerView.ViewHolder (itemView) {
             fun bind(email : Email) {
                 with(email) {
-                    itemView.findViewById<TextView>(R.id.text_user).text = user.first().toString()
+                    val hash = user.hashCode()
+                    itemView.findViewById<TextView>(R.id.text_icon).text = user.first().toString()
+                    itemView.findViewById<TextView>(R.id.text_icon).background = itemView.oval(Color.rgb(hash, hash / 2,0))
+                    itemView.findViewById<TextView>(R.id.text_user).text = user
+                    itemView.findViewById<TextView>(R.id.text_subject).text = subject
+                    itemView.findViewById<TextView>(R.id.text_preview).text = preview
+                    itemView.findViewById<TextView>(R.id.text_date).text = date
+
+                    itemView.findViewById<TextView>(R.id.text_user).setTypeface(Typeface.DEFAULT, if (unread) BOLD else NORMAL)
+                    itemView.findViewById<TextView>(R.id.text_subject).setTypeface(Typeface.DEFAULT, if (unread) BOLD else NORMAL)
+                    itemView.findViewById<TextView>(R.id.text_date).setTypeface(Typeface.DEFAULT, if (unread) BOLD else NORMAL)
+
+                    itemView.findViewById<ImageView>(R.id.img_star).setImageResource(
+                        if (stared) R.drawable.ic_baseline_star_24
+                        else R.drawable.ic_baseline_star_border_24
+                    )
+
 
                 }
             }
@@ -33,4 +57,14 @@ class EmailAdapter(
     override fun getItemCount(): Int {
         return emails.size
     }
+}
+
+fun View.oval(@ColorInt color: Int): ShapeDrawable {
+    val oval = ShapeDrawable(OvalShape())
+    with(oval) {
+        intrinsicHeight = height
+        intrinsicWidth = width
+        paint.color = color
+    }
+    return oval
 }
