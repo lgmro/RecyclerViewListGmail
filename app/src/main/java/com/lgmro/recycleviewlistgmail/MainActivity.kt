26fe9e3b865c2865e2e733ce7d3f71f5bbee7f26
lgmro <lucasgmmro@gmail.com>
2022-03-16 19:52:29 -0300

    Add function to add emails with Fakeit

--- app/src/main/java/com/lgmro/recycleviewlistgmail/MainActivity.kt
+++ app/src/main/java/com/lgmro/recycleviewlistgmail/MainActivity.kt
@@ -2,19 +2,52 @@ package com.lgmro.recycleviewlistgmail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.lgmro.recycleviewlistgmail.model.Email
+import com.lgmro.recycleviewlistgmail.model.email
 import com.lgmro.recycleviewlistgmail.model.fakeEmails
+import com.mooveit.library.Fakeit
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: EmailAdapter
+    private val recycler get() = findViewById<RecyclerView>(R.id.recycle_view_main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fakeit.init()
         setContentView(R.layout.activity_main)
 
-        val recycler = findViewById<RecyclerView>(R.id.recycle_view_main)
-        recycler.adapter = EmailAdapter(fakeEmails())
+
+
+        adapter = EmailAdapter(fakeEmails())
+        recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(this)
 
 
+    }
+
+    fun addEmail(view: View) {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")).parse(
+            Fakeit.dateTime().dateFormatter()
+        )
+        adapter.emails.add(0, email {
+            stared = false
+            unread = true
+            user = Fakeit.name().firstName()
+            subject = Fakeit.company().name()
+            date = SimpleDateFormat("d MMM", Locale("pt", "BR")).format(sdf)
+            preview = mutableListOf<String>().apply {
+                repeat(10) {
+                    add(Fakeit.lorem().words())
+                }
+            }.joinToString(" ")
+        })
+        adapter.notifyItemInserted(0)
+
+        recycler.scrollToPosition(0)
     }
 }
